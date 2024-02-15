fa860119c5b4e72e36c15ce84c7b48ab289fdec8
Katherine <hurstkatherine@hotmail.co.uk>
2024-02-15 18:56:08 +0000

    player adder added

--- src/components/PlayerAdder.jsx
+++ src/components/PlayerAdder.jsx
@@ -1,11 +1,20 @@
-const PlayerAdder = () => {
-  const [nameInput, setNameInput] = "";
+import { useState } from "react";
+
+const PlayerAdder = ({ setInitList }) => {
+  const [formInput, setFormInput] = useState({ name: "", init: "", mod: "" });
   const handleSubmit = (event) => {
     event.preventDefault();
+    setInitList((currInitList) => {
+      const newPlayer = {};
+    });
   };
 
-  const handleChange = (event, value) => {
-    
+  const handleChange = (event, key) => {
+    setFormInput((currFormInput) => {
+      const formInputCopy = { ...currFormInput };
+      formInputCopy[key] = event.target.value;
+      return formInputCopy;
+    });
   };
 
   return (
@@ -14,15 +23,29 @@ const PlayerAdder = () => {
       <input
         type="text"
         id="name"
-        value={nameInput}
+        value={formInput.name}
         onChange={(event) => {
           handleChange(event, "name");
         }}
       ></input>
-      <label for="dex">Dex</label>
-      <input type="text" id="dex"></input>
       <label for="init">Init</label>
-      <input type="text" id="init"></input>
+      <input
+        type="text"
+        id="init"
+        value={formInput.init}
+        onChange={(event) => {
+          handleChange(event, "init");
+        }}
+      ></input>
+      <label for="mod">Modifier</label>
+      <input
+        type="text"
+        id="mod"
+        value={formInput.mod}
+        onChange={(event) => {
+          handleChange(event, "mod");
+        }}
+      ></input>
       <button type="submit">Submit</button>
     </form>
   );
